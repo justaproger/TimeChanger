06b748e660051467ec8d6ebf4992be225000be1a
unknown <justaproger@gmail.com>
2023-12-26 02:42:01 +0500

    'first'

--- main.py
+++ main.py
@@ -1,29 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image, ImageDraw, ImageFont
+import datetime
 import asyncio
-from datetime import datetime
+from telethon.sync import TelegramClient
+from telethon.tl import functions, types
 import pytz
-from telethon import TelegramClient, functions
 
-api_id = '1839181'
-api_hash = 'd4cd79a3fb11d5fe2d827d0b93219778'
-session = 'your_session'
+# Функция для склонения слов
+def plural_form(n, forms):
+    if n%10==1 and n%100!=11:
+        return forms[0]
+    elif 2<=n%10<=4 and (n%100<10 or n%100>=20):
+        return forms[1]
+    else:
+        return forms[2]
 
-async def time_until_new_year():
-    tashkent_tz = pytz.timezone('Asia/Tashkent')
-    now = datetime.now(tashkent_tz)
-    new_year = datetime(year=now.year + 1, month=1, day=1, tzinfo=tashkent_tz)
-    delta = new_year - now
-    days, seconds = delta.days, delta.seconds
-    hours = seconds // 3600
-    minutes = (seconds % 3600) // 60
-    return days, hours, minutes
+async def create_image():
+    # Создаем клиент Telegram
+    client = TelegramClient('my_session', api_id='1839181', api_hash='d4cd79a3fb11d5fe2d827d0b93219778')
+    await client.start()
 
-async def change_last_name_every_minute():
-    async with TelegramClient(session, api_id, api_hash) as client:
-        while True:
-            days, hours, minutes = await time_until_new_year()
-            last_name = f"До Нового года осталось \n {days} дней, {hours} часов и {minutes} минут(ы)."
-            await client(functions.account.UpdateProfileRequest(first_name=last_name))
-            await asyncio.sleep(60)
+    # Устанавливаем часовой пояс
+    tz = pytz.timezone('Asia/Tashkent')
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(change_last_name_every_minute())
+    while True:
+        # Создаем изображение с черным фоном
+        img = Image.new('RGB', (500, 500), color = (0, 0, 0))
+
+        d = ImageDraw.Draw(img)
+        fnt = ImageFont.truetype("font.ttf", 32, encoding='UTF-8')
+
+        # Вычисляем, сколько времени осталось до Нового года
+        now = datetime.datetime.now(tz)
+        new_year = datetime.datetime(year=now.year+1, month=1, day=1, tzinfo=tz)
+        remaining = new_year - now
+        
+        # Форматируем оставшееся время без секунд
+        days = remaining.days
+        hours, remainder = divmod(remaining.seconds, 3600)
+        minutes, _ = divmod(remainder, 60)
+        remaining_str = f'{days} {plural_form(days, ["день", "дня", "дней"])} {hours} {plural_form(hours, ["час", "часа", "часов"])} {minutes} {plural_form(minutes, ["минута", "минуты", "минут"])}'
+
+        # Вычисляем размеры текста
+        text_width, text_height = d.textsize(remaining_str, font=fnt)
+
+        # Вычисляем позицию текста
+        text_x = (img.width - text_width) / 2
+        text_y = (img.height - text_height) / 2
+
+        # Добавляем текст на изображение
+        d.text((text_x, text_y), f'Осталось до Нового года:\n {remaining_str}', font=fnt, fill=(0, 255, 0))
+
+        # Сохраняем изображение
+        img.save('newyear.png')
+
+        # Получаем текущие профильные фотографии
+        photos = await client.get_profile_photos('me')
+
+        # Если есть профильные фотографии, удаляем последнюю
+        if photos:
+            await client(functions.photos.DeletePhotosRequest(id=[photos[0]]))
+
+        # Загружаем новую профильную фотографию
+        file = await client.upload_file('newyear.png')
+        await client(functions.photos.UploadProfilePhotoRequest(file=file))
+
+        # Ждем одну минуту
+        await asyncio.sleep(60)
+
+    await client.stop()
+
+# Запускаем функцию create_image
+asyncio.run(create_image())
